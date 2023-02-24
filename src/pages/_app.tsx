6387cc0076f89779f92def62bd4da0e62af3a21e
renancorreadev <producer@skynance.net>
2023-02-23 23:42:33 -0300

    feat: initialize project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../UI/styles/global.css';
+
+import type { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Next.js + TypeScript</h1>
+    </div>
+  );
+};
+
+export default Index;
