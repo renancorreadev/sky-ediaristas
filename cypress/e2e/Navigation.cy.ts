7a060ca71a4e3ef0ac3ea4b09b15a0a79272f494
renancorreadev <producer@skynance.net>
2023-02-24 18:13:36 -0300

    chore: configure jest & cypress testings

---
+++ cypress/e2e/Navigation.cy.ts
@@ -0,0 +1,8 @@
+describe('Navigation', () => {
+  describe('Static pages', () => {
+    it('should navigate to the about page', () => {
+      // Start from the index page
+      cy.visit('/');
+    });
+  });
+});
