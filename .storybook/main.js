0f9b3b1f736fe2d67d40293223ad207b019078c7
renancorreadev <producer@skynance.net>
2023-02-27 17:35:56 -0300

    chore: configure typescript paths in module of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,15 @@
 const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const toPath = (_path) => path.join(process.cwd(), _path)
 
 module.exports = {
   webpackFinal: async (config) => {
     config.resolve.modules.push(toPath('src'));
-
-    config.resolve.alias['@emotion/core'] = toPath('node_modules/@emotion/react')
-    config.resolve.alias['@emotion/styled'] = toPath('node_modules/@emotion/styled')
-    config.resolve.alias['@emotion-theming'] = toPath('node_modules/@emotion/react')
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    config.resolve.alias['@emotion/core'] = toPath('node_modules/@emotion/react');
+    config.resolve.alias['@emotion/styled'] = toPath('node_modules/@emotion/styled');
+    config.resolve.alias['@emotion-theming'] = toPath('node_modules/@emotion/react');
 
     return config;
   },
