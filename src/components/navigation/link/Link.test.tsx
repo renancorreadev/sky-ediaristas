69c8bc887350b06ca175f89ee2cf0379c602cdeb
renancorreadev <producer@skynance.net>
2023-02-28 01:07:33 -0300

    test: create test for navigation links

---
+++ src/components/navigation/link/Link.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+
+import { mockRouter } from '@/tests/mocks';
+
+import Link from './Link';
+
+describe('Test Link component', () => {
+  beforeEach(() => {
+    mockRouter;
+  });
+
+  test('should render a link with href and children', () => {
+    render(<Link href="/home">Home</Link>);
+
+    const link = screen.getByRole('link', { name: 'Home' });
+
+    expect(link).toHaveAttribute('href', '/home');
+    expect(link).toHaveTextContent('Home');
+  });
+
+  test('should render a button link with MuiLinkProps', () => {
+    render(
+      <Link href="/contact" mui={{ variant: 'contained' }}>
+        Contact Us
+      </Link>
+    );
+
+    const buttonLink = screen.getByRole('link', { name: 'Contact Us' });
+
+    expect(buttonLink).toHaveAttribute('href', '/contact');
+    expect(buttonLink).toHaveTextContent('Contact Us');
+    expect(buttonLink).toHaveClass(
+      'MuiTypography-root MuiTypography-contained MuiLink-root MuiLink-underlineAlways css-1ps4owl-MuiTypography-root-MuiLink-root '
+    );
+  });
+});
