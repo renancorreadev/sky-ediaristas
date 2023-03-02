f91d8b98889286abdc69a2d68dd05ece8bbb5146
renancorreadev <producer@skynance.net>
2023-03-02 17:09:15 -0300

    feat: ensure create frequent questions component

---
+++ src/UI/partials/index/_frequent-question.tsx
@@ -0,0 +1,86 @@
+import {
+  AccordionDetails,
+  AccordionSummary,
+  Container,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+
+import {
+  AccondionStyled,
+  SectionContainer,
+  SectionSubTitle,
+  SectionTitle,
+  Wave,
+} from './_frequent-question.styled';
+
+const questionsList = [
+  {
+    question: 'Dúvida 1',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptates saepe et, maiores cupiditate ratione rerum voluptatum dolores alias ipsa sint tempore molestiae hic. Sit assumenda nisi in labore nihil.',
+  },
+  {
+    question: 'Dúvida 2',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptates saepe et, maiores cupiditate ratione rerum voluptatum dolores alias ipsa sint tempore molestiae hic. Sit assumenda nisi in labore nihil.',
+  },
+  {
+    question: 'Dúvida 3',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptates saepe et, maiores cupiditate ratione rerum voluptatum dolores alias ipsa sint tempore molestiae hic. Sit assumenda nisi in labore nihil.',
+  },
+  {
+    question: 'Dúvida 4',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptates saepe et, maiores cupiditate ratione rerum voluptatum dolores alias ipsa sint tempore molestiae hic. Sit assumenda nisi in labore nihil.',
+  },
+];
+
+const FrequestQuestion = () => {
+  const [activeAccordion, setActiveAccordion] = useState(0);
+
+  function isOpen(accordionIndex: number): boolean {
+    return activeAccordion === accordionIndex;
+  }
+
+  function changeOpenAccordion(accordionIndex: number): void {
+    if (isOpen(accordionIndex)) {
+      setActiveAccordion(0);
+    } else {
+      setActiveAccordion(accordionIndex);
+    }
+  }
+
+  function getIcon(accordionIndex: number): string {
+    return isOpen(accordionIndex) ? 'twf-minus' : 'twf-plus';
+  }
+
+  return (
+    <SectionContainer>
+      <Wave src={'/img/home/waves.svg'} />
+      <Container>
+        <SectionTitle>Ainda está com dúvidas?</SectionTitle>
+        <SectionSubTitle>Veja abaixo as perguntas frequentes</SectionSubTitle>
+
+        {questionsList.map((item, index) => (
+          <AccondionStyled
+            key={index}
+            expanded={isOpen(index + 1)}
+            onChange={() => changeOpenAccordion(index + 1)}
+          >
+            <AccordionSummary expandIcon={<i className={getIcon(index + 1)} />}>
+              <Typography color={'primary'}>{item.question}</Typography>
+            </AccordionSummary>
+
+            <AccordionDetails>
+              <Typography>{item.answer}</Typography>
+            </AccordionDetails>
+          </AccondionStyled>
+        ))}
+      </Container>
+    </SectionContainer>
+  );
+};
+
+export default FrequestQuestion;
